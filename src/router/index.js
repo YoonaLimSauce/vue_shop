fb374db2fd4744574713b5538cb3dd3e11a8313c
yoonalimsauce <miraclefishleong@gmail.com>
2024-03-11 23:47:06 +0800

    vue ui中vue-shop项目进行精简初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,18 @@
+/*
+ * @Author: yoonalimsauce miraclefishleong@gmail.com
+ * @Date: 2024-03-11 22:54:47
+ * @LastEditors: yoonalimsauce miraclefishleong@gmail.com
+ * @LastEditTime: 2024-03-11 23:45:44
+ * @FilePath: \vue-shop\src\router\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  /* */
 ]
 
 const router = new VueRouter({
