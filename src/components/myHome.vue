81ee56a49ee3d4e0a1393dbcb026a7130423831f
yoonalimsauce <miraclefishleong@gmail.com>
2024-03-13 00:09:14 +0800

    使用.replace()替换.push()，因为replace不会留下历史记录，不能回退至前一个页面，更安全

--- src/components/myHome.vue
+++ src/components/myHome.vue
@@ -2,7 +2,7 @@
  * @Author: yoonalimsauce miraclefishleong@gmail.com
  * @Date: 2024-03-12 23:36:03
  * @LastEditors: yoonalimsauce miraclefishleong@gmail.com
- * @LastEditTime: 2024-03-13 00:04:42
+ * @LastEditTime: 2024-03-13 00:08:09
  * @FilePath: \vue-shop\src\components\Home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -18,7 +18,8 @@ export default {
     logout () {
       this.$message.success('退出成功！')
       window.sessionStorage.clear()
-      this.$router.push('/login')
+      // this.$router.push('/login')
+      this.$router.replace('/login')
     }
   }
 }
