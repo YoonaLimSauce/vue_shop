e57d3e446c7eed51d4a95de7c8d8ad53d3046cd5
林允儿 Yoona Lim <miraclefishleong@gmail.com>
2024-03-16 01:11:35 +0800

    给http请求头配置token，使用axios发起http请求，对应配置

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * @Author: yoonalimsauce miraclefishleong@gmail.com
  * @Date: 2024-03-11 22:54:47
  * @LastEditors: 林允儿 Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-03-16 00:14:13
+ * @LastEditTime: 2024-03-16 01:08:42
  * @FilePath: \vue-shop\src\main.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,6 +20,13 @@ import axios from 'axios'
 
 /* 配置请求的根路径 */
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
+axios.interceptors.request.use(
+  (config) => {
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+    /* 在最后必须 return config */
+    return config
+  }
+)
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
