1828a7602e95b961c2aef3617a854f5f68c2670e
林允儿 Yoona Lim <miraclefishleong@gmail.com>
2024-03-15 23:47:01 +0800

    开始学习使用elementUi进行首页布局 导入Container, Header, Main三个elementUi组件并设计相应的css样式

--- src/components/myHome.vue
+++ src/components/myHome.vue
@@ -1,15 +1,35 @@
 <!--
  * @Author: yoonalimsauce miraclefishleong@gmail.com
  * @Date: 2024-03-12 23:36:03
- * @LastEditors: yoonalimsauce miraclefishleong@gmail.com
- * @LastEditTime: 2024-03-13 00:08:09
+ * @LastEditors: 林允儿 Yoona Lim miraclefishleong@gmail.com
+ * @LastEditTime: 2024-03-15 23:42:28
  * @FilePath: \vue-shop\src\components\Home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <!-- 这里是首页的布局 -->
+  <el-container class="home-container">
+    <!-- 这里是首页的头部-->
+    <el-header>
+      <!-- 这里是首页的头部内容-->
+      Header
+      <el-button type="info" @click="logout">
+        退出
+      </el-button>
+    </el-header>
+    <!-- 这里是首页的主体-->
+    <el-container>
+      <!-- 这里是首页的侧边栏-->
+      <el-aside width="200px">
+        <!-- 这里是首页的侧边栏内容-->
+        Aside
+      </el-aside>
+      <el-main>
+        <!-- 这里是首页的主要内容-->
+        Main
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -26,5 +46,16 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
+  .home-container {
+    height: 100%;
+  } 
+  .el-header {
+    background-color: #373D41;
+  }
+  .el-aside {
+    background-color: #333744;
+  }
+  .el-main {
+    background-color: #eaedf1;
+  }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Footer, Menu } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Footer)
+Vue.use(Menu)
+
 Vue.prototype.$message = Message
