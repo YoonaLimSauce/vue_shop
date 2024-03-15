15c8f32f1d167789ca22275cb2a85a528e6d172a
林允儿 Yoona Lim <miraclefishleong@gmail.com>
2024-03-16 00:12:46 +0800

    完成主页Header布局，Css样式设置

--- src/components/myHome.vue
+++ src/components/myHome.vue
@@ -2,7 +2,7 @@
  * @Author: yoonalimsauce miraclefishleong@gmail.com
  * @Date: 2024-03-12 23:36:03
  * @LastEditors: 林允儿 Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-03-15 23:42:28
+ * @LastEditTime: 2024-03-16 00:12:04
  * @FilePath: \vue-shop\src\components\Home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -12,7 +12,10 @@
     <!-- 这里是首页的头部-->
     <el-header>
       <!-- 这里是首页的头部内容-->
-      Header
+      <div>
+        <img src="../assets/heima.png" alt="logo">
+        <span>电商后台管理系统</span>
+      </div>
       <el-button type="info" @click="logout">
         退出
       </el-button>
@@ -48,9 +51,22 @@ export default {
 <style lang="less" scoped>
   .home-container {
     height: 100%;
-  } 
+  }
   .el-header {
     background-color: #373D41;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 20px;
+    color: #fff;
+    font-size: 20px;
+    > div {
+      display: flex;
+      align-items: center;
+      span {
+        margin-left: 15px;
+      }
+    }
   }
   .el-aside {
     background-color: #333744;
