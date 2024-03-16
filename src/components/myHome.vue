a1ffc1af5d404a1df77a2f22ac6759c4f9a4c767
Yoona Lim <miraclefishleong@gmail.com>
2024-03-17 00:53:58 +0800

    收拾现场

--- src/components/myHome.vue
+++ src/components/myHome.vue
@@ -2,7 +2,7 @@
  * @Author: yoonalimsauce miraclefishleong@gmail.com
  * @Date: 2024-03-12 23:36:03
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-03-17 00:42:47
+ * @LastEditTime: 2024-03-17 00:53:24
  * @FilePath: \vue-shop\src\components\Home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
