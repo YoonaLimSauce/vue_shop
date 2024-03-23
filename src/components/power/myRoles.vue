f2414e257213a58397db555971679d7f6b8f0c46
Yoona Lim <miraclefishleong@gmail.com>
2024-03-23 15:18:16 +0800

    使用数据库接口获取RolesList对应的数据

---
+++ src/components/power/myRoles.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>角色列表</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rolesList: []
+    }
+  },
+  created() {
+    this.getRolesList()
+  },
+  methods: {
+    async getRolesList() {
+      // TODO: 获取角色列表
+      const { data: result } = await this.$http.get('roles')
+      if (result.meta.status !== 200) {
+        return this.$message.error(result.meta.message)
+      }
+      this.rolesList = result.data
+      console.log(this.rolesList)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
