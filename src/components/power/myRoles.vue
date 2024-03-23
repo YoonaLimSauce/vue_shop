cf7c44a7472ae6ffb12734a8000094f769fbb608
Yoona Lim <miraclefishleong@gmail.com>
2024-03-23 23:19:57 +0800

    创建角色列表表格并添加展开列

--- src/components/power/myRoles.vue
+++ src/components/power/myRoles.vue
@@ -1,6 +1,37 @@
 <template>
   <div>
-    <h3>角色列表</h3>
+    <!-- 面包屑导航 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图-->
+    <el-card>
+      <el-row>
+        <el-col>
+          <el-button type="primary">添加角色</el-button>
+        </el-col>
+      </el-row>
+
+      <!-- 角色列表区域 -->
+      <el-table :data="rolesList" style="width: 100%" border stripe>
+        <!-- 展开列 -->
+        <el-table-column type="expand"></el-table-column>
+        <!-- 索引列 -->
+        <el-table-column type="index" label="ID"></el-table-column>
+        <el-table-column prop="roleName" label="角色名称"></el-table-column>
+        <el-table-column prop="roleDesc" label="角色描述"></el-table-column>
+        <el-table-column label="操作">
+          <template slot-scope="">
+            <el-button size="mini" type="primary">编辑</el-button>
+            <el-button size="mini" type="danger">删除</el-button>
+            <el-button size="mini" type="warning">分配权限</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
   </div>
 </template>
 
@@ -16,7 +47,7 @@ export default {
   },
   methods: {
     async getRolesList() {
-      // TODO: 获取角色列表
+      // 获取角色列表
       const { data: result } = await this.$http.get('roles')
       if (result.meta.status !== 200) {
         return this.$message.error(result.meta.message)
