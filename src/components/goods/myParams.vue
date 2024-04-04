c6f1d46e9584683353c3a6a129a12cedd3401b4b
Yoona Lim <miraclefishleong@gmail.com>
2024-04-05 00:38:39 +0800

    动态显示分类数据，绑定级联选择器及标签切换显示

--- src/components/goods/myParams.vue
+++ src/components/goods/myParams.vue
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-01 00:31:17
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-04 12:39:03
+ * @LastEditTime: 2024-04-04 22:32:11
  * @FilePath: \vue_shop\src\components\goods\myParams.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -16,7 +16,7 @@
       <el-breadcrumb-item>参数列表</el-breadcrumb-item>
     </el-breadcrumb>
 
-    <!-- 卡片视图区域-->
+    <!-- 卡片视图区域 -->
     <el-card>
       <!-- 警告提示 -->
       <el-alert title="注意：只允许为第三级分类设置相关参数！！！" type="warning"
@@ -36,13 +36,43 @@
 
       <!-- Tab标签选项卡切换区域 -->
       <el-tabs v-model="activeTabName" @tab-click="handleTabClick">
-        <!-- 添加动态参数的面板-->
-        <el-tab-pane label="动态参数" name="only">
-          <el-button type="primary" size="mini" :disabled="isButtonDisabled">动态参数</el-button>
+        <!-- 添加动态参数的面板 -->
+        <el-tab-pane label="动态参数" name="many">
+          <el-button type="primary" size="mini" :disabled="isButtonDisabled">添加动态参数</el-button>
+          <!-- 动态参数表格 -->
+          <el-table :data="manyTableData" border stripe
+            :key="componentKey" :ref="tableForm">
+            <!-- 展开行 -->
+            <el-table-column type="expand"></el-table-column>
+            <!-- 索引列 -->
+            <el-table-column type="index"></el-table-column>
+            <el-table-column prop="attr_name" label="参数名称"></el-table-column>
+            <el-table-column label="操作">
+              <template>
+                <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+                <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
-        <!-- 添加静态属性的面板-->
-        <el-tab-pane label="静态属性" name="many">
-          <el-button type="primary" size="mini" :disabled="isButtonDisabled">静态属性</el-button>
+        <!-- 添加静态属性的面板 -->
+        <el-tab-pane label="静态属性" name="only">
+          <el-button type="primary" size="mini" :disabled="isButtonDisabled">添加静态属性</el-button>
+          <!-- 静态属性表格 -->
+          <el-table :data="onlyTableData" border stripe
+            :key="componentKey" :ref="tableForm">
+            <!-- 展开行 -->
+            <el-table-column type="expand"></el-table-column>
+            <!-- 索引列 -->
+            <el-table-column type="index"></el-table-column>
+            <el-table-column prop="attr_name" label="属性名称"></el-table-column>
+            <el-table-column label="操作">
+              <template>
+                <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+                <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
       </el-tabs>
     </el-card>
@@ -55,6 +85,7 @@ export default {
   data () {
     return {
       // Data properties go here
+      componentKey: 0,
       // 商品分类级联选择器配置
       cateProps: {
         label: 'cat_name',
@@ -68,7 +99,7 @@ export default {
       // 选中的商品分类
       selectedCateKeys: [],
       // 当前激活的tab标签
-      activeTabName: 'only',
+      activeTabName: 'many',
       // 动态参数表格数据
       manyTableData: [],
       // 静态属性表格数据
@@ -89,6 +120,7 @@ export default {
       }
       this.cateList = result.data
     },
+    // 获取参数列表数据
     async getParamsData() {
       // 如果选中的商品分类不是第三级分类，清空选中的商品分类
       if (this.selectedCateKeys.length !== 3) {
