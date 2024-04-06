c4cad88c68a769ec5fb1c5cd8230425f21dc1ba6
Yoona Lim <miraclefishleong@gmail.com>
2024-04-06 23:30:29 +0800

    处理显示bug，当切换二级一级分类菜单时，不会清空当前页面所显示的分类数据，同时没有禁用相关按钮操作，存在缺陷风险

--- src/components/goods/myParams.vue
+++ src/components/goods/myParams.vue
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-01 00:31:17
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-06 23:18:13
+ * @LastEditTime: 2024-04-06 23:23:42
  * @FilePath: \vue_shop\src\components\goods\myParams.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -256,6 +256,8 @@ export default {
       // 如果选中的商品分类不是第三级分类，清空选中的商品分类
       if (this.selectedCateKeys.length !== 3) {
         this.selectedCateKeys = []
+        this.manyTableData = []
+        this.onlyTableData = []
         return
       }
 
