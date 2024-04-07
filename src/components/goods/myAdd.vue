e0b76c2f186782bd18baba4c711c21057b86f51b
Yoona Lim <miraclefishleong@gmail.com>
2024-04-07 22:32:51 +0800

    设置添加商品页面的面包屑导航及卡片视图，并在卡片视图中添加alert警告信息组件以及steps进度条组件

--- src/components/goods/myAdd.vue
+++ src/components/goods/myAdd.vue
@@ -1,7 +1,30 @@
+<!--
+ * @Author: Yoona Lim miraclefishleong@gmail.com
+ * @Date: 2024-04-07 22:19:27
+ * @LastEditors: Yoona Lim miraclefishleong@gmail.com
+ * @LastEditTime: 2024-04-07 22:31:06
+ * @FilePath: \vue_shop\src\components\goods\myAdd.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
 <template>
   <div>
     <!-- Component HTML goes here -->
-    添加商品页面
+    <!-- Breadcrumb面包屑导航 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/welcome' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>添加商品</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- Card卡片视图 -->
+    <el-card>
+      <el-alert title="添加商品信息" type="info" center show-icon :closable="false"></el-alert>
+      <el-steps :space="200" :active="1" finish-status="success">
+        <el-step title="已完成"></el-step>
+        <el-step title="进行中"></el-step>
+        <el-step title="步骤 3"></el-step>
+      </el-steps>
+    </el-card>
   </div>
 </template>
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -11,7 +11,7 @@ import {
   Button, Form, FormItem, Input, Message, Tag, Tree, Select, Option, Cascader, Alert,
   Container, Header, Aside, Main, Footer, Menu, Submenu, MenuItem, MenuItemGroup,
   Breadcrumb, BreadcrumbItem, Card, Row, Col, Table, TableColumn, TabPane, Tabs,
-  Switch, Tooltip, Pagination, Dialog, MessageBox
+  Switch, Tooltip, Pagination, Dialog, MessageBox, Steps, Step
 } from 'element-ui'
 
 Vue.use(Button)
@@ -46,6 +46,8 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
