0dd1abe6d6e24e1363689417711afb3acbcf02c0
Yoona Lim <miraclefishleong@gmail.com>
2024-04-11 23:04:45 +0800

    商品详情页面双向绑定form表单数据，随意更改会导致错误，导入lodash依赖插件进行数据深拷贝后进行数据处理

--- src/components/goods/myAdd.vue
+++ src/components/goods/myAdd.vue
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-07 22:19:27
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-11 22:39:14
+ * @LastEditTime: 2024-04-11 23:03:23
  * @FilePath: \vue_shop\src\components\goods\myAdd.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -98,6 +98,7 @@
 </template>
 
 <script>
+import lodash from 'lodash'
 export default {
   // Component data, props, methods, etc, go here
   data () {
@@ -171,6 +172,18 @@ export default {
   },
   methods: {
     // Component methods go here
+    // 添加商品
+    async addGoods() {
+      this.$refs.addFormRef.validate(
+        (valid) => {
+          if (!valid) {
+            return this.$message.error('请检查必要的表单数据')
+          }
+          const form = lodash.cloneDeep(this.addForm)
+          form.goods_cat = form.goods_cat.join(',')
+        }
+      )
+    },
     // 获取所有商品分类数据
     async getCateList() {
       const { data: result } = await this.$http.get('/categories', {
@@ -182,15 +195,6 @@ export default {
         return this.$message.error('获取商品分类数据失败')
       }
       this.cateList = result.data
-    },
-    async addGoods() {
-      this.$refs.addFormRef.validate(
-        (valid) => {
-          if (!valid) {
-            return this.$message.error('请检查表单数据')
-          }
-        }
-      )
     },
     async tabClicked() {
       if (this.activeIndex === '1') {
